8fdffe42a003fdb39ffc2c31903187747c3ba37e
Eric Potash <eric@k2co3.net>
2023-02-17 19:05:30 -0500

    getting somewhere

--- isd.sh
+++ isd.sh
@@ -1,6 +1,11 @@
-./mirror_isd.sh www `python ./station_ids.py config/stations_in.csv`
+export ISD_QUICK=1
 
-#rm -f pipe0
-#mkfifo pipe0
-#./isd2csv.sh www > pipe0 & psql -1f isd2psql.sql
-#rm pipe0
+STATION_IDS=`python3 ./station_ids.py config/stations_in.csv`
+
+./mirror_isd.sh www $STATION_IDS
+
+for STATION_ID in $STATION_IDS
+do
+    mkdir -p csv/$STATION_ID
+    ./isd2csv.sh www $STATION_ID | python3 isd2csv.py csv/$STATION_ID
+done
