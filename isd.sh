d9afc79b4354b54246a4cf13f5805fff1ae3632a
Eric Potash <eric@k2co3.net>
2023-04-29 13:40:32 -0400

    latest

--- isd.sh
+++ isd.sh
@@ -1,11 +1,10 @@
-#export ISD_QUICK=1
-
+# fully mirror the ISD data and generate summary CSVs
 STATION_IDS=`python3 ./station_ids.py config/stations_in.csv`
 
 ./mirror_isd.sh www $STATION_IDS
 
 for STATION_ID in $STATION_IDS
 do
-    mkdir -p csv/$STATION_ID
-    python3 isd_summary.py www csv $STATION_ID
+    mkdir -p csv/isd/$STATION_ID
+    python3 isd_summary.py www csv/isd $STATION_ID
 done
