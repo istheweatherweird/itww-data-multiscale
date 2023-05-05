bd2b4a67e5ef02646901d9628284a3825ca64840
Eric Potash <eric@k2co3.net>
2023-05-05 11:28:52 -0400

    old station

--- isd.py
+++ isd.py
@@ -4,7 +4,8 @@ import utils
 WWW_DIR = sys.argv[1]
 OUT_DIR = sys.argv[2]
 station_id = sys.argv[3]
+old_station_id = utils.get_old_station_id(station_id)
 
-df = utils.read_isd(WWW_DIR, station_id)
+df = utils.read_isd(WWW_DIR, station_id, old_station_id)
 summary = utils.get_isd_summary(df)
 utils.write_isd_summary(summary, OUT_DIR + "/" + station_id)
--- station_ids.py
+++ station_ids.py
@@ -4,6 +4,9 @@ import os
 
 stations_in = pd.read_csv(sys.argv[1], dtype=str)
 
+if "ISD_QUICK" in os.environ.keys():
+    stations_in = stations_in.head(1)
+
 ids = stations_in.USAF + "-" + stations_in.WBAN
 
 if "ISD_LATEST" not in os.environ.keys():
