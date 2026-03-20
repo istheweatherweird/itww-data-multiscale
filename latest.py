87432c006c241869a437aa3e3252e04226d3f5e1
Eric Potash <eric@k2co3.net>
2026-03-20 13:36:36 -0400

    print latest

--- latest.py
+++ latest.py
@@ -11,6 +11,7 @@ df_ghcnh = utils.read_ghcnh_parquet(WWW_DIR, station_id)
 ICAO = utils.get_ICAO(station_id)
 last_ghcnh_timestamp = df_ghcnh.sort_index().index[-1]
 df_latest = utils.get_latest(ICAO, last_ghcnh_timestamp - pd.Timedelta(1, "h"))
+print(df_latest)
 
 df = utils.combine_ghcnh_latest(df_ghcnh, df_latest)
 
